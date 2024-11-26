dec8654c11dedf457a0eb5900c576221868b8645
Rerezz Official <thegamergemu0001@gmail.com>
2024-11-26 15:49:44 +0700

    Update main.js

--- main.js
+++ main.js
@@ -19,3 +19,14 @@ function toggleSidebar() {
     overlay.style.display = 'none'; // Sembunyikan overlay
   }
   
+  document.getElementById('sendEmailButton').addEventListener('click', function() {
+    var nama = document.getElementById('nama').value;
+    var email = document.getElementById('email').value;
+    var pesan = document.getElementById('pesan').value;
+
+    // Membuat URL mailto dengan informasi form yang sudah diisi
+    var mailtoLink = "mailto:rerezzofficial@gmail.com.com?subject=Pesan%20Dari%20Website%20dari%20" + encodeURIComponent(nama) + "&body=" + encodeURIComponent("Email: " + email + "\nPesan: " + pesan);
+
+    // Membuka email client dengan link mailto
+    window.location.href = mailtoLink;
+});
